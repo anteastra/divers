6b190be3a4fd175356b293c6dc1a5470410cb6d0
anteastra <anteastra@gmail.com>
2013-09-29 01:06:28 +0700

    change code to object style

--- program.js
+++ program.js
@@ -1,43 +1,46 @@
-var ctx="";
-var ctx2="";
-var back = new Image();
-back.src = 'images/back.jpg';
-var diver = new Image();
-diver.src = 'images/diver.png';
+﻿var diverGame = {
+  ctx: "",
+  ctx2: "",
+  back: new Image(),
+  diver: new Image(),
+  dwidth: 66,
+  dheight: 63,
+  dframes: 2, 
+  dcurrentFrame: 0,
+  diverX: 20,
+  diverY: 400,
 
-var dwidth = 66, 
-        dheight = 63, 
-        dframes = 2, 
-          
-        dcurrentFrame = 0;
+  func_init: function(){
+    this.back.src = 'images/back.jpg';
+    this.diver.src = 'images/diver.png';
+  },
 
-var diverX = 20;
-var diverY = 400;
+  func_draw: function(){
+    this.func_init();
+    var canvas = document.getElementById('tutorial');
+    this.ctx="";
+    var canvas2 = document.getElementById('back');
+    this.ctx2=canvas2.getContext('2d');
+    this.ctx2.drawImage(this.back,0,0);
+    if (canvas.getContext) {
+      this.ctx = canvas.getContext('2d');
+    }
+    var self = this;
+    setInterval(function(){self.func_render();},1000/60);
+  },
 
-function draw(){
-  var canvas = document.getElementById('tutorial');
-  ctx="";
-  var canvas2 = document.getElementById('back');
-  ctx2=canvas2.getContext('2d');
-  ctx2.drawImage(back,0,0);
-  if (canvas.getContext) {
-    ctx = canvas.getContext('2d');
-  }
-  setInterval(render,1000/60);
-}
-
-
-var tick=0;
-function render(){
-	
-	ctx.clearRect(diverX, diverY, dwidth, dheight); 
-	diverX++;
-	if (diverX>675) diverX = 20;
-    ctx.drawImage(diver, 0, dheight * dcurrentFrame, dwidth, dheight, diverX, diverY, dwidth, dheight); 
-        if (dcurrentFrame === dframes) { 
-          dcurrentFrame = 0; 
+  func_render: function(){
+    this.ctx.clearRect(this.diverX, this.diverY, this.dwidth, this.dheight); 
+    this.diverX++;
+    this.diverX++;
+    this.diverX++;
+    if (this.diverX>675) this.diverX = 20;
+    this.ctx.drawImage(this.diver, 0, this.dheight * this.dcurrentFrame, this.dwidth, this.dheight, this.diverX, this.diverY, this.dwidth, this.dheight); 
+        if (this.dcurrentFrame === this.dframes) { 
+          this.dcurrentFrame = 0; 
         } else { 
-          dcurrentFrame++; 
-        } 
-	
+          this.dcurrentFrame++; 
+        }
+  }
 }
+
